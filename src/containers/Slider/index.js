2a7dbbee64667b448ad66dd2bca43c60f9f660f0
nei7uj <julien.dille@gmail.com>
2025-04-09 15:27:54 +0200

    Retour arrière slider page blanche

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -11,13 +11,14 @@ const Slider = () => {
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
+    setTimeout(
+      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      5000
+    );
   };
   useEffect(() => {
-    const intervalId = setInterval(nextCard, 5000);
-    
-    return () => clearInterval(intervalId);
-  }, []);
+    nextCard();
+  });
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
