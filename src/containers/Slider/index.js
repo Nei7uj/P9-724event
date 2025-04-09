8384403361f9996e0f9c13cd5d8f74964497bbdc
nei7uj <julien.dille@gmail.com>
2025-04-09 15:04:37 +0200

    Correction bug slider décroissant et index

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,17 +8,16 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
   };
   useEffect(() => {
-    nextCard();
-  });
+    const intervalId = setInterval(nextCard, 5000);
+    
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
